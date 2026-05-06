18e4af65df964fb76e6c72a371a8e2f1387265d5
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-06 11:06:07 +0530

    add page counts

--- src/analyzer.py
+++ src/analyzer.py
@@ -11,3 +11,17 @@ def get_status_counts(data):
             status_counts[status] = 1
 
     return status_counts
+
+def get_top_page(data):
+    page_counts = {}
+    for item in data:
+        path =  item["path"]
+
+        if path in page_counts:
+            page_counts[path] += 1
+        else:
+            page_counts[path] = 1
+
+    top_page = max(page_counts, key=page_counts.get)
+
+    return top_page
