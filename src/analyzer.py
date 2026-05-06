a8d94e86f86a6ccee584586d4c512c7750fd80be
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-06 10:00:41 +0530

    integrate analyzer with main flow

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,2 +1,13 @@
 def get_total_requests(data):
     return len(data)
+
+def get_status_counts(data):
+    status_counts = {}
+    for item in data:
+        status = item["status"]
+        if status in status_counts:
+            status_counts[status] += 1
+        else:
+            status_counts[status] = 1
+
+        return status_counts
